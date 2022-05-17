c48263b06fb01ef2dd5a628d581f4bc4d77ab1d4
Matthias Gatto <matthias.gatto@outscale.com>
2022-05-17 14:22:41 +0200

    add missing file
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ cognac-completion-tpl.bash
@@ -0,0 +1,38 @@
+#/usr/bin/env bash
+
+____func_code____
+
+_mk_profiles()
+{
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    if [ -f ~/.osc/config.json ]; then
+        PROFILES=$(cat ~/.osc/config.json | tr -d '\n:'  | sed 's/{[^{}]*}//g' | tr -d "{}\" " | sed 's/,/ /g')
+    elif [ -f ~/.osc_sdk/config.json ]; then
+        PROFILES=$(cat ~/.osc_sdk/config.json | tr -d '\n:'  | sed 's/{[^{}]*}//g' | tr -d "{}\" " | sed 's/,/ /g')
+    fi
+    for x in $PROFILES ; do echo --profile=$x ; done
+}
+
+_cognac()
+{
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+
+    case ${COMP_CWORD} in
+        *)
+            case ${prev} in
+		____piped_call_list____)
+		    eval ${prev}
+		    ;;
+                *)
+                    COMPREPLY=($(compgen -W "____call_list____" -- ${cur}))
+		    ;;
+            esac
+            ;;
+    esac
+}
+
+complete -F _cognac cognac
+# this one is for debug
+complete -F _cognac ./cognac
